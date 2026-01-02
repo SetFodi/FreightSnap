3daf29efbe4c881d3316e4841b4f2679e9b8306a
SetFodi <lukafartenadze2004@gmail.com>
2026-01-02 11:13:43 +0400

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { ToastProvider } from "@/components/toast";
+import { Analytics } from "@vercel/analytics/next";
 
 const spaceGrotesk = Space_Grotesk({
     subsets: ["latin"],
@@ -28,6 +29,7 @@ export default function RootLayout({
                 suppressHydrationWarning
             >
                 <ToastProvider>{children}</ToastProvider>
+                <Analytics />
             </body>
         </html>
     );
